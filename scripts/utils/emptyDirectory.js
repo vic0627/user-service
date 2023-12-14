4c5c46f915bada969142792fc105e242814a24b0
DESKTOP-GMTST5N\Sunrise <pure90719@gmail.com>
2023-12-14 16:58:39 +0800

    add protected target to empty dir function

--- scripts/utils/emptyDirectory.js
+++ scripts/utils/emptyDirectory.js
@@ -6,30 +6,48 @@ const readdir = promisify(fs.readdir);
 const unlink = promisify(fs.unlink);
 const rmdir = promisify(fs.rmdir);
 
+const protectedPaths = ["src", ".husky", ".git", "docs", "example", "scripts"];
+const protectedFiles = [
+  ".eslintrc.cjs",
+  ".gitignore",
+  ".prettierignore",
+  ".prettierrc",
+  "babel.config.js",
+  "jest.config.js",
+  "package-lock.json",
+  "package.json",
+  "README.md",
+  "tsconfig.json",
+];
+
+/**
+ * @param {string[]} targetList
+ * @param {string} target
+ */
+function hasProtectedTarget(targetList, target) {
+  return targetList.some((forbid) => target.includes(forbid));
+}
+
 module.exports = async function emptyDirectory(...resolvePath) {
   if (!resolvePath?.length) throw new Error("paths required");
 
   try {
-    // 組合完整的目錄路徑
     const fullPath = path.resolve(...resolvePath);
 
-    // 讀取目錄中的所有檔案
+    if (hasProtectedTarget(protectedPaths, fullPath))
+      throw new Error(`Path '${fullPath}' contains forbidden directory`);
+
     const files = await readdir(fullPath);
 
-    // 遍歷所有檔案，並刪除它們
     for (const file of files) {
+      if (protectedFiles.includes(file)) throw new Error(`failed to delete protected file '${file}'`);
+
       const filePath = path.join(fullPath, file);
 
-      // 判斷是否為檔案還是目錄
       const isDirectory = fs.statSync(filePath).isDirectory();
 
-      // 如果是目錄，遞迴清空目錄
-      if (isDirectory) {
-        await emptyDirectory(filePath);
-      } else {
-        // 如果是檔案，直接刪除
-        await unlink(filePath);
-      }
+      if (isDirectory) await emptyDirectory(filePath);
+      else await unlink(filePath);
     }
 
     // 刪除完畢後，刪除原始目錄
@@ -39,9 +57,10 @@ module.exports = async function emptyDirectory(...resolvePath) {
   } catch (error) {
     timeLog(error.message);
 
-    if (error.message.includes("ENOENT: no such file or directory, scandir '/Users/pure90719/code/user-service/dist'"))
-      return true;
+    const distNotExisit = error.message?.includes(
+      "ENOENT: no such file or directory, scandir '/Users/pure90719/code/user-service/dist'",
+    );
 
-    return false;
+    return !!distNotExisit;
   }
 };
