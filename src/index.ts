5a91c64d0ea55b1bb7ebc50104554dcbb6f12f25
victor <pure90719@gmail.com>
2023-12-14 08:31:39 +0800

    modify doc of user interface

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,56 @@ const us = new UserService(
   {} as ServiceFormData,
 );
 
-export const { scheduledTask } = us;
+/**
+ * ## ScheduledTask
+ * 
+ * ScheduledTask serves as a scheduler for managing and executing tasks at specified intervals.
+ * The module provides functionality to add tasks, both anonymous and named, to a task list.
+ * These tasks are then executed periodically based on a configurable interval.
+ * 
+ * ### key Features
+ * 
+ * 1. Task Management:
+ * 
+ *    - The module maintains a collection of tasks using a Map structure.
+ *    - Tasks can be either anonymous (non-named) or named, allowing for flexibility in task management.
+ * 
+ * 2. Interval Configuration:
+ * 
+ *    - The execution interval for scheduled tasks is configurable,
+ *      allowing users to set the frequency at which tasks are executed.
+ *    - The default interval is set to 10 minutes (1000 * 60 * 10 milliseconds).
+ * 
+ * 3. Task Execution:
+ * 
+ *    - The execute method triggers the immediate execution of tasks.
+ *    - Tasks are executed asynchronously at regular intervals based on the configured interval.
+ * 
+ * 4. Task Addition:
+ * 
+ *    - Tasks can be added to the scheduler using the addTask method.
+ *      These tasks are assigned random names and are added to the
+ *      task list regardless of whether similar tasks already exist.
+ *    - Singleton tasks, added using the addSingletonTask method,
+ *      are named tasks that are only added if a task with the same name does not already exist in the task list.
+ * 
+ * 5. Interval Adjustment:
+ * 
+ *    - The setInterval method allows for the dynamic adjustment of the execution interval.
+ *      It performs validation to ensure that the interval is a positive number and that the
+ *      adjustment is not attempted when the service has a parent (presumably, a Service instance).
+ * 
+ * 6. Task Execution Control:
+ * 
+ *    - The module starts and stops the scheduling of tasks based on the presence of tasks in the task list.
+ *      The scheduling is controlled by an interval timer.
+ * 
+ * 7. Task Cleanup:
+ * 
+ *    - The clearSchedule method stops the scheduling of tasks and clears the entire task list.
+ * 
+ */
+export const scheduledTask = us.scheduledTask;
 
 /**
  * encapsulate API routes or endpoints and generate a root service
@@ -127,14 +176,158 @@ export const defineType = us.defineType.bind(us);
  * @param rules valid rules
  * @returns token of the intersection rule, directly assign it to any property of the rule object
  *
- * @todo continue doc writing...
+ * @example
+ *
+ * // 1. get the token by defining intersection rules
+ * const forbiddenString = defineIntersection('string', (prop,  value) => ({
+ *    valid: !value?.includes('foobar'),
+ *    msg: `Parameter '${prop}' with '${value}' for value contains a forbidden word 'foobar'`
+ * }))
+ *
+ * // 2. pass the return value to a rule object's property
+ * export default {
+ *    // ...
+ *    api: {
+ *      name: 'thisIsAMethodName',
+ *      body: { title: 'this is a parameter of this method' }
+ *      // parameter ' should be a string and cannot includes 'foobar'
+ *      // parameter 'title' should be a string and cannot includes 'foobar'
+ *      rules: { title: forbiddenString },
+ *    }
+ * }
  */
 export const defineIntersection = us.defineIntersection.bind(us);
+/**
+ * Treat multiple rules as the union of sets
+ *
+ * @param rules valid rules
+ * @returns token of the union rule, directly assign it to any property of the rule object
+ *
+ * @example
+ *
+ * // 1. get the token by defining union rules
+ * const singleChar = defineUnion('string@1', 'int@0:9')
+ *
+ * // 2. pass the return value to a rule object's property
+ * export default {
+ *    // ...
+ *    api: {
+ *      name: 'thisIsAMethodName',
+ *      body: { symbol: 'this is a parameter of this method' }
+ *      // parameter 'symbol' should be a single character or an integer 0 to 9
+ *      rules: { symbol: singleChar },
+ *    }
+ * }
+ */
 export const defineUnion = us.defineUnion.bind(us);
+/**
+ * combine multiple rule objects into a new object
+ *
+ * @description this function will neithor alter the requirement of properties nor mutate the given objects in-place
+ *
+ * @param targets valid rule objects
+ * @returns combinated object of all given objects
+ *
+ * @example
+ *
+ * // 1. define two different rule objects
+ * const ruleA = { userName: 'string@1:10' }
+ * const ruleB = { age: 'int@0:120' }
+ *
+ * // 2. merge two rules
+ * const mixture = mergeRules(ruleA, ruleB)
+ * console.log(mixture)
+ * // {
+ * //   userName: 'string@1:10'
+ * //   age: 'int@0:120'
+ * // }
+ */
 export const mergeRules = us.mergeRules.bind(us);
+/**
+ * Copy all properties from the given object to a new object, and make all the properties optional
+ *
+ * @param target valid rule object
+ * @returns new object with optional properties
+ *
+ * @example
+ *
+ * // 1. define rule object with required properties
+ * const required = {
+ *    email: 'string',
+ *    password: 'string'
+ * }
+ *
+ * // 2. create a same rule object with optional properties
+ * const partial = partialRules(required)
+ * console.log(partial)
+ * // {
+ * //   $email: 'string',
+ * //   $password: 'string'
+ * // }
+ */
 export const partialRules = us.partialRules.bind(us);
+/**
+ * Copy all properties from the given object to a new object, and make all the properties required
+ *
+ * @param target valid rule object
+ * @returns new object with required properties
+ *
+ * @example
+ *
+ * // 1. define rule object with optional properties
+ * const optional = {
+ *    $email: 'string',
+ *    $password: 'string'
+ * }
+ *
+ * // 2. create a same rule object with required properties
+ * const required = requiredRules(optional)
+ * console.log(required)
+ * // {
+ * //   email: 'string',
+ * //   password: 'string'
+ * // }
+ */
 export const requiredRules = us.requiredRules.bind(us);
+/**
+ * Copy all properties from the given object to a new object, except selected keys
+ *
+ * @param target valid rule object
+ * @param args keys that should be omited
+ *
+ * @example
+ *
+ * // 1. define a rule object
+ * const userRules = {
+ *    name: 'string',
+ *    $age: 'int@1:120'
+ * }
+ *
+ * // 2. create a new rule object by omiting keys from original object
+ * const nameRule = omitRules(userRules, 'age') // => you don't need to mind about the requirements of the prop
+ * console.log(nameRule)
+ * // { name: 'string' }
+ */
 export const omitRules = us.omitRules.bind(us);
+/**
+ * Copy all selected properties from the given object to a new object
+ *
+ * @param target valid rule object
+ * @param args keys that should be selected
+ *
+ * @example
+ *
+ * // 1. define a rule object
+ * const userRules = {
+ *    name: 'string',
+ *    $age: 'int@1:120'
+ * }
+ *
+ * // 2. create a new rule object by omiting keys from original object
+ * const nameRule = pickRules(userRules, 'name')
+ * console.log(nameRule)
+ * // { name: 'string' }
+ */
 export const pickRules = us.pickRules.bind(us);
 
 export default us;
